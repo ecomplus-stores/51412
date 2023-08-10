bdeaf47c1c1b7edc9b16edb78498c6d1b8eb4b7a
victorsilvacode <victorsilva.code@gmail.com>
2023-08-10 18:08:07 -0300

    development features

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,2 +1,24 @@
 // Add your custom JavaScript for storefront pages here.
+// ALL PAGES
 
+const allPages = {
+    actionsScroll(){
+        $(window).scroll(function () {
+            const scrollTop = $(this).scrollTop();
+
+            if (scrollTop > 20) {
+                $('body').addClass('header-fixed');
+            } else {
+                $('body').removeClass('header-fixed');
+            }
+        });
+
+        $(window).scroll();
+    },
+
+    init(){
+        this.actionsScroll();
+    }
+}
+
+allPages.init();
