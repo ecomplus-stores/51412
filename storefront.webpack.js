a9a6296910ba9266c3b8911f33d1c8c4d5c4e1b1
Leonardo Matos <leomp120894@gmail.com>
2023-08-24 16:48:16 -0300

    Copying APrices html

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,7 +5,8 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-      './sections': path.resolve(__dirname, 'template/js/custom-js/cms/sections.js')
+      './sections': path.resolve(__dirname, 'template/js/custom-js/cms/sections.js'),
+      './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/components/html/APrices.html')
     }
   }
 })
